239aaf51ac2081a43d53d5c3f1f7be2779c40322
Marc-Eric Pelletier <medaragon@hotmail.com>
2021-12-13 23:15:04 -0500

    Puzzle 3.2

--- 2021/day03.js
+++ 2021/day03.js
@@ -16,7 +16,7 @@ var testInput = [
 ]
 
 var myInput = fileReader.readFile('./2021/day03-data.txt')
-console.log(myInput)
+// console.log(myInput)
 
 
 const length = myInput.length
@@ -27,6 +27,42 @@ myInput[0].split('').forEach((_, i) => {
 })
 const epsilon = gamma.map(x => Number(!x))
 const parse = (arr) => parseInt(arr.join().replaceAll(',', ''), 2)
-console.log(parse(gamma))
-console.log(parse(epsilon))
-console.log(parse(gamma) * parse(epsilon))
+// console.log(parse(gamma))
+// console.log(parse(epsilon))
+const powerConsumption = parse(gamma) * parse(epsilon)
+console.log(powerConsumption)
+
+const getCounts = (arr) => arr.reduce((a, c) => {
+  a[c] = (a[c] || 0) + 1;
+  return a;
+}, {});
+
+const getMostFrequent = (arr) => {
+  const counts = getCounts(arr)
+  const mostFrequents = Object.keys(counts).filter(k => counts[k] === Math.max(...Object.values(counts)))
+  return Math.max(...mostFrequents.map(x => Number(x)))
+}
+const getLessFrequent = (arr) => {
+  const counts = getCounts(arr)
+  const lessFrequents = Object.keys(counts).filter(k => counts[k] === Math.min(...Object.values(counts)))
+  return Math.min(...lessFrequents.map(x => Number(x)))
+}
+
+const getRating = (input, chooseMostFrequent) => {
+  var remainingList = input
+  for (var i = 0; i < input[0].length; i++) {
+    const bitsAtCurrentIndex = remainingList.map(x => x.split('')[i])
+    const chosenBit = chooseMostFrequent ? getMostFrequent(bitsAtCurrentIndex) : getLessFrequent(bitsAtCurrentIndex)
+    remainingList = remainingList.filter(x => Number(x[i]) === Number(chosenBit))
+    // console.log('Chosen bit : ' + chosenBit + ' Numbers left : ' + remainingList)
+    if (remainingList.length === 1) {
+      return remainingList[0]
+    }
+  }
+}
+
+const oxygenGeneratorRating = parse(getRating(myInput, true).split(''))
+const co2ScrubberRating = parse(getRating(myInput, false).split(''))
+const lifeSupportRating = oxygenGeneratorRating * co2ScrubberRating
+
+console.log(lifeSupportRating)
