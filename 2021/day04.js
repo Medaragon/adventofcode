09faa7a12c40379aca10ba6a7418107171bd7e04
Marc-Eric Pelletier <medaragon@hotmail.com>
2022-01-12 22:14:04 -0500

    Puzzle 4.2

--- 2021/day04.js
+++ 2021/day04.js
@@ -34,15 +34,33 @@ const checkDrawnNumberOnCard = (drawnNumber, cards) => {
   })
 }
 
-const checkIfACardWon = (cards) => {
-  const cardsVictoryState = cards.map(c => {
-    const checkIfCardWinsInARow = (card) => card.some(row => row.every(num => num.checked))
-    const checkIfCardWinsInAColumn = (card) => card.some((row, i) => card.every(c => c[i].checked))
+const getCardsVictoryState = (cards) => cards.map(c => {
+  const checkIfCardWinsInARow = (card) => card.some(row => row.every(num => num.checked))
+  const checkIfCardWinsInAColumn = (card) => card.some((row, i) => card.every(c => c[i].checked))
 
-    return checkIfCardWinsInARow(c) || checkIfCardWinsInAColumn(c)
-  })
+  return checkIfCardWinsInARow(c) || checkIfCardWinsInAColumn(c)
+})
+
+const checkIfACardWon = (cards) => {
+  const cardsVictoryState = getCardsVictoryState(cards)
   
-  return cardsVictoryState.findIndex(x => x === true)
+  return cardsVictoryState.findIndex(x => x)
+}
+
+const checkAllCardsHavewon = (cards) => {
+  const cardsVictoryState = getCardsVictoryState(cards)
+  
+  return cardsVictoryState.every(x => x)
+}
+
+const checkIfAllButOneCardWon = (cards) => {
+  const cardsVictoryState = getCardsVictoryState(cards)
+
+  if (cardsVictoryState.filter(x => !x).length === 1) {
+    return cardsVictoryState.findIndex(x => !x)
+  }
+
+  return -1;
 }
 
 const drawNumbersUntilAWin = (drawnNumbers, initialBingoCards) => {
@@ -52,13 +70,28 @@ const drawNumbersUntilAWin = (drawnNumbers, initialBingoCards) => {
     const winningCardIndex = checkIfACardWon(bingoCards)
 
     if (winningCardIndex !== -1) {
-      displayBingoCards(bingoCards)
+      // displayBingoCards(bingoCards)
 
       return [bingoCards[winningCardIndex], drawnNumbers[i]]
     }
   }
 
-  return null
+  return [null, null]
+}
+
+const drawNumbersUntilAllWin = (drawnNumbers, initialBingoCards) => {
+  const bingoCards = initialBingoCards
+  let losingCardIndex = -1
+  for(let i = 0; i < drawnNumbers.length; i++) {
+    checkDrawnNumberOnCard(drawnNumbers[i], bingoCards)
+    if (losingCardIndex === -1) {
+      losingCardIndex = checkIfAllButOneCardWon(bingoCards)
+    } else if (checkAllCardsHavewon(bingoCards)) {
+      return [bingoCards[losingCardIndex], drawnNumbers[i]]
+    }
+  }
+
+  return [null, null]
 }
 
 const calculateScoreOfWinningCard = (winningCard, lastDrawnNumber) => {
@@ -73,7 +106,7 @@ var input = myInput
 var drawnNumbers = input.shift().split(',')
 var initialBingoCards = getBingoCards(input)
 
-
+console.log('Puzzle 1')
 const [winningCard, lastDrawnNumber] = drawNumbersUntilAWin(drawnNumbers, initialBingoCards)
 if (!winningCard) {
   console.log('No one won')
@@ -82,3 +115,11 @@ if (!winningCard) {
   console.log(finalScore)
 }
 
+console.log('Puzzle 2')
+const [losingCard, lastDrawnNumber2] = drawNumbersUntilAllWin(drawnNumbers, initialBingoCards)
+if (!losingCard) {
+  console.log('No one lost')
+} else {
+  const finalScore = calculateScoreOfWinningCard(losingCard, lastDrawnNumber2)
+  console.log(finalScore)
+}
